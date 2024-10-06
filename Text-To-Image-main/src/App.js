5ecd69731458f4087aaa414ae7d87f33e3f084dd
aakash-sharma25 <as024harma@gmail.com>
2024-10-06 11:46:31 +0530

    final commit

---
+++ Text-To-Image-main/src/App.js
@@ -0,0 +1,11 @@
+import ImageGenerationForm from "./components/ImageGen";
+import "./index.css";
+function App() {
+  return (
+    <div className=" bg-[#3D52A0] h-[100vh]">
+      <ImageGenerationForm />
+    </div>
+  );
+}
+
+export default App;
